f32929a400bfcbb301913dbcdee4185262a03b47
Holger Seelig <holger.seelig@gmail.com>
2020-12-21 10:56:20 +0100

    Added window title.

--- xTraceRoute/AppDelegate.swift
+++ xTraceRoute/AppDelegate.swift
@@ -9,31 +9,35 @@ import Cocoa
 import SwiftUI
 
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-   var window: NSWindow!
-
-
-   func applicationDidFinishLaunching(_ aNotification: Notification) {
+class AppDelegate :
+   NSObject,
+   NSApplicationDelegate
+{
+   var window : NSWindow!
+   
+   func applicationDidFinishLaunching (_ aNotification : Notification)
+   {
       // Create the SwiftUI view that provides the window contents.
-      let contentView = ContentView()
-
+      let contentView = ContentView ()
+      
       // Create the window and set the content view.
-      window = NSWindow(
-          contentRect: NSRect(x: 0, y: 0, width: 480, height: 480),
-          styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-          backing: .buffered, defer: false)
-      window.isReleasedWhenClosed = false
-      window.center()
-      window.setFrameAutosaveName("Main Window")
-      window.contentView = NSHostingView(rootView: contentView)
-      window.makeKeyAndOrderFront(nil)
+      window = NSWindow (contentRect: NSRect (x: 0, y: 0, width: 480, height: 480),
+                         styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+                         backing: .buffered,
+                         defer: false)
+      
+      window .title                = "xTraceRoute"
+      window .isReleasedWhenClosed = false
+      window .contentView          = NSHostingView (rootView: contentView)
+      
+      window .setFrameAutosaveName ("Main Window")
+      window .center ()
+      window .makeKeyAndOrderFront (nil)
    }
-
-   func applicationWillTerminate(_ aNotification: Notification) {
+   
+   func applicationWillTerminate (_ aNotification : Notification)
+   {
       // Insert code here to tear down your application
    }
-
-
 }
 
